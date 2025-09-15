1c3847477df2950f5509d9ff5663645cebabcc9f
Jeremy Heng <hengweyseing53@gmail.com>
2025-09-15 13:18:47 +0800

    flink source table

--- flink/1_catelog_glue.sql
+++ flink/1_catelog_glue.sql
@@ -1,10 +1,3 @@
--- -- jar dependencies
--- ADD JAR '/opt/flink/lib/iceberg-aws-bundle-1.9.2.jar';
--- ADD JAR '/opt/flink/lib/iceberg-flink-runtime-1.19-1.9.2.jar';
--- ADD JAR '/opt/flink/lib/hadoop-aws-3.3.6.jar';
--- ADD JAR '/opt/flink/lib/hadoop-client-api-3.3.6.jar';
--- ADD JAR '/opt/flink/lib/hadoop-client-runtime-3.3.6.jar';
-
 -- glue catalog
 CREATE CATALOG catalog_glue WITH (
     'type'='iceberg',
@@ -13,8 +6,48 @@ CREATE CATALOG catalog_glue WITH (
     'io-impl'='org.apache.iceberg.aws.s3.S3FileIO'
 );
 
+-- preset
 USE CATALOG catalog_glue;
 USE iceberg_db1;
 
-SELECT * FROM `iceberg_db1`.`order` ORDER BY update_date DESC LIMIT 5;
+-- source table
+CREATE TABLE kafka_source_order (
+  `id` INT,
+  `product` STRING,
+  `amount` INT,
+  `buyer_id` INT,
+  `create_date` STRING,
+  `update_date` STRING
+) WITH (
+  'connector' = 'kafka',
+  'topic' = 'stream_order_intake',
+  'properties.bootstrap.servers' = 'broker:29092',
+  'properties.group.id' = 'flink-iceberg-consumer-20250915-1000',
+  'format' = 'json', 
+  'scan.startup.mode' = 'earliest-offset'
+);
 
+-- SET 'sql-client.execution.result-mode' = 'changelog';
+
+-- CREATE TABLE test_topic_raw (
+--   message STRING
+-- ) WITH (
+--   'connector' = 'kafka',
+--   'topic' = 'test_topic',
+--   'properties.bootstrap.servers' = 'broker:29092',
+--   'properties.group.id' = 'flink_raw_debug',
+--   'scan.startup.mode' = 'earliest-offset',
+--   'format' = 'json'
+-- );
+
+-- CREATE TABLE test_topic_raw (
+--   raw_value STRING
+-- ) WITH (
+--   'connector' = 'kafka',
+--   'topic' = 'test_topic',
+--   'properties.bootstrap.servers' = 'broker:29092',
+--   'properties.group.id' = 'flink_raw_debug_20250915_1100',
+--   'scan.startup.mode' = 'earliest-offset',
+--   'format' = 'raw',
+--   'raw.charset' = 'UTF-8'
+-- );
